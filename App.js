3d8247318d3877d575a73b56ef4c3e10e09bdc52
susanwinters214 <susanwinters214@gmail.com>
2020-08-28 09:27:46 -0500

    Added form Friends

--- App.js
+++ App.js
@@ -1,14 +1,43 @@
 const express = require("express");
 var app = express();
 const ejs = require('ejs');
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
+
+// ADD TO LIST:
+var friends = ["Charlie", "Bentley", "Tilly", "Jacob" ];
 
 app.get("/", function(req, res){
     res.render("home.ejs")
    // res.send("<h1>Welcome to the home page</h1>");
 });
+
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Post 1", author: "Susan"},
+        {title: "Post 2", author: "Charlie"},
+        {title: "Post 3", author: "Tina"},
+    ];
+    res.render('posts.ejs', {posts:posts}); // called in template // variable
+})
+
+app.post("/addfriend", function(req, res){
+    var newFriend = req.body.newFriend;
+    console.log("NEW FRIEND: " + newFriend);
+   friends.push(newFriend);
+  res.redirect("/friends");    
+  }); 
+
+
+app.get("/friends", function(req, res){
+
+       res.render('friends.ejs', {friends:friends}); // called in template // variable
+  });
+
+ 
 
 // Empty Page for Template
 app.get("/empty", function(req, res){
@@ -34,8 +63,8 @@ app.get("/posts", function(req, res){
 
 
 
-app.listen(3200, function(){
-    console.log("Server has started on port 3200");
+app.listen(3300, function(){
+    console.log("Server has started on port 3300");
 });
 
 /*
--- readme.md
+++ readme.md
@@ -7,9 +7,9 @@ Extremely Basic EJS Website Template with Basic Routing SetUp in App.js
 
 Views Folder 
 Partials include: header, topnav and footer 
-1 simple page loops thru an array of items and displays them
-in an HTML UL / LI format using javaScript inside of an EJS template. 
-1 dynamic page displays 
+1 page (posts) with a for loop that loops thru an array of items and displays them in an HTML UL / LI format using javaScript inside of an EJS template. 
+
+1 page (friends) contains a list of items (coming from an array defined in App.js) and a form at the bottom of the page. When you enter and item into the form and hit the button, it will add your item to the existing list of items because array.push is defined in App.js in the route.  
 
 Start with: node App:
 You'll start off by seeing how the page is divided into sections.
